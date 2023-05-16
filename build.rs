dc44199924c50e3fd44a6d610345a38073f7dc64
Genrikh Fetishchev <genrikhfetischev@gmail.com>
2023-05-16 14:03:21 +0200

    looks like fixed build for macos and linux

--- build.rs
+++ build.rs
@@ -5,38 +5,23 @@ use std::path::{PathBuf};
 use std::process::Command;
 
 fn main() {
-  let cur_dir = env::current_dir().unwrap();
   let host = env::var("HOST").unwrap();
   let target = env::var("TARGET").unwrap();
+  let is_windows = host.contains("windows") && target.contains("windows");
+  if is_windows {
+    panic!("Windows is not currently supported");
+  }
+
+  let is_apple = host.contains("apple") && target.contains("apple");
+  let is_linux = host.contains("linux") && target.contains("linux");
+
+
+  let cur_dir = env::current_dir().unwrap();
+
   let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
   let uws_dir = cur_dir.join("uWebSockets");
   let us_dir = uws_dir.join("uSockets");
   let capi_dir = uws_dir.join("capi");
-  let is_windows = host.contains("windows") && target.contains("windows");
-  if is_windows {
-    panic!("uws-sys is not support windows yet");
-  }
-
-  if !uws_dir.exists() {
-    if !Command::new("git")
-      .args(["submodule", "update", "--init", "uWebSockets"])
-      .status()
-      .unwrap()
-      .success()
-    {
-      panic!("Can't pull uWebSockets")
-    };
-
-    if !Command::new("git")
-      .current_dir(uws_dir)
-      .args(["submodule", "update", "--init", "uSockets"])
-      .status()
-      .unwrap()
-      .success()
-    {
-      panic!("Can't pull uSockets")
-    }
-  }
 
   bindgen::Builder::default()
     .clang_arg(format!("-I{}", us_dir.join("src").display()))
@@ -47,52 +32,52 @@ fn main() {
     .write_to_file(out_dir.join("bindings.rs"))
     .expect("Couldn't write bindings!");
 
-  #[cfg(feature = "uws_vendored")]
-  if !Command::new("make")
-    .current_dir(&capi_dir)
-    .arg("capi")
-    .status()
-    .expect("Failed to run make")
-    .success()
-  {
-    panic!("Can't build UWS CAPI!");
-  }
 
-
-  let is_apple = host.contains("apple") && target.contains("apple");
-  let is_linux = host.contains("linux") && target.contains("linux");
+  println!("cargo:rustc-link-lib=z");
+  println!("cargo:rustc-link-lib=uv");
+  println!("cargo:rustc-link-lib=ssl");
+  println!("cargo:rustc-link-lib=crypto");
 
 
   if is_apple {
     println!("cargo:rustc-link-lib=c++");
-
-    #[cfg(feature = "uws_vendored")]
-    {
-      let status = Command::new("libtool")
-        .current_dir(&capi_dir)
-        .arg("-static")
-        .arg("-o")
-        .arg("libuwebsockets.a")
-        .arg("libuwebsockets.o")
-        .arg("../uSockets/uSockets.a")
-        .status()
-        .expect("Failed to run libtool")
-        .success();
-
-      if !status {
-        panic!("Failed to pack libuwebsockets.a");
-      }
-    }
+  } else if is_linux {
+    println!("cargo:rustc-link-lib=stdc++");
   }
 
-  if is_linux {
-    println!("cargo:rustc-link-search=native=/usr/lib/gcc/aarch64-linux-gnu/11");
-    println!("cargo:rustc-link-lib=stdc++");
 
-    #[cfg(feature = "uws_vendored")]
+  // Remove libuwebsockets.a built by make because to my understanding it has packed in a wrong way
+  #[cfg(feature = "uws_vendored")]
+  {
+    if !Command::new("make")
+      .current_dir(&capi_dir)
+      .arg("capi")
+      .status()
+      .expect("Failed to run make")
+      .success()
     {
-      let _ = Command::new("rm").current_dir(&capi_dir).arg("-f").arg("libuwebsockets.a").status().expect("failed to delete lib").success();
-      let mut o_files = read_dir_by_condition(&us_dir, |file_name| file_name.ends_with(".o"));
+      panic!("Can't build UWS CAPI!");
+    }
+
+    let _ = Command::new("rm").current_dir(&capi_dir).arg("-f").arg("libuwebsockets.a").status().expect("failed to delete lib").success();
+
+    let mut o_files = read_dir_by_condition(&us_dir, |file_name| file_name.ends_with(".o"));
+    let success = if is_apple {
+      let mut args = vec![
+        "-static".to_string(),
+        "-o".to_string(),
+        capi_dir.join("libuwebsockets.a").to_str().unwrap().to_string(),
+        capi_dir.join("libuwebsockets.o").to_str().unwrap().to_string(),
+      ];
+      args.append(&mut o_files);
+
+      Command::new("libtool")
+        .current_dir(&capi_dir)
+        .args(args)
+        .status()
+        .expect("Failed to run libtool")
+        .success()
+    } else if is_linux {
       let mut args = vec![
         "rcs".to_string(),
         "-o".to_string(),
@@ -101,34 +86,23 @@ fn main() {
       ];
       args.append(&mut o_files);
 
-      let status = Command::new("ar")
+      Command::new("ar")
         .current_dir(&out_dir)
         .args(args)
         .status()
         .expect("Failed to pack libuwebsockets")
-        .success();
+        .success()
+    } else { false };
 
-      if !status {
-        panic!("Failed to pack libuwebsockets.a");
-      }
+    if !success {
+      panic!("Failed to pack libuwebsockets.a");
     }
 
+    copy_by_condition(capi_dir, &out_dir, |filename| filename.ends_with(".a"));
 
-    println!("cargo:rustc-link-lib=stdc++");
-    #[cfg(feature = "uws_vendored")]
     println!("cargo:rustc-link-search=native={}", out_dir.display());
-    #[cfg(feature = "uws_vendored")]
     println!("cargo:rustc-link-lib=uwebsockets");
   }
-
-
-  #[cfg(feature = "uws_vendored")]
-  copy_by_condition(capi_dir, &out_dir, |filename| filename.ends_with(".a"));
-
-  println!("cargo:rustc-link-lib=z");
-  println!("cargo:rustc-link-lib=uv");
-  println!("cargo:rustc-link-lib=crypto");
-  println!("cargo:rustc-link-lib=ssl");
 }
 
 #[cfg(feature = "uws_vendored")]
